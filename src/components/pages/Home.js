9cefc53f1af832cc1fd1c50f2d7c3aa797869b31
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2021-04-20 20:11:20 +0530

    Fetched data from the api

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React , { useState , useEffect } from 'react';
+
 
 const Home = () => {
+    const [newsArticles , setNewsArticles] = useState([])
+    const [loading , setLoading] = useState(false)
+
+    useEffect(() => {
+        const getNewsArticles = () => {
+            try {
+                setLoading(true)
+                fetch('https://api.jsonbin.io/b/607ee47cf099765deef85e5d')
+                .then(res => res.json()).then(({articles}) => {
+                    setNewsArticles(articles)
+                    setLoading(false)
+                    console.log(articles)
+                })
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
+
+        getNewsArticles()
+    }, [])
     return (
         <div>
-            This is a home component
+            {
+                (newsArticles && loading) ? <h3>Loading...</h3>:(
+                    newsArticles.map((newsArticle , index) => (
+                        <div key = {index}>
+                        <h3>{newsArticle.author}</h3>
+                        </div>
+                    ))
+                ) 
+            }
         </div>
     )
 }
