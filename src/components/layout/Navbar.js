e9b462a40678bec612ac8773dca8df759c4ef85b
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2021-04-21 16:31:42 +0530

    Added logout method

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { Fragment, useContext } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import AuthContext from '../../context/auth/authContext'
 
 const Navbar = ({ title , icon }) => {
+
+    const authContext = useContext(AuthContext)
+
+    const {isAuthenticated , logout , user} = authContext;
+
+    const onLogout = () => {
+        logout();
+    }
+
+    const authLinks = (
+        <Fragment>
+            <li>Hello {user && user.name}</li>
+            <li>
+                <a href='#!' onClick={onLogout}>
+                    <i className='fas fa-sign-out-alt'></i>
+                    <span className='hide-sm'>
+                        Logout
+                    </span>
+                </a>
+            </li>
+        </Fragment>
+    );
+    
+        const guestLinks = (
+            <Fragment>
+                <li>
+                    <Link to = '/register'>Register</Link>
+                </li>
+                <li>
+                    <Link to = '/login'>Login</Link>
+                </li>
+            </Fragment>
+        )
+
     return (
         <div className='navbar bg-primary'>
             <h1>
@@ -15,12 +50,7 @@ const Navbar = ({ title , icon }) => {
                 <li>
                     <Link to = '/saved'>Saved News</Link>
                 </li>
-                <li>
-                    <Link to = '/register'>Register</Link>
-                </li>
-                <li>
-                    <Link to = '/login'>Login</Link>
-                </li>
+                {isAuthenticated ? authLinks : guestLinks}
             </ul>
             
         </div>
